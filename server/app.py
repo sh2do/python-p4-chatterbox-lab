95a84e6ff9c49de9a1b6b9928ab0f7e3100986d0
Zaionix <sddeh5981@gmail.com>
2026-01-12 20:15:55 +0300

    donr

--- server/app.py
+++ server/app.py
@@ -14,13 +14,56 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        return jsonify([{
+            'id': message.id,
+            'body': message.body,
+            'username': message.username,
+            'created_at': message.created_at.isoformat(),
+            'updated_at': message.updated_at.isoformat()
+        } for message in messages])
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        
+        return jsonify({
+            'id': new_message.id,
+            'body': new_message.body,
+            'username': new_message.username,
+            'created_at': new_message.created_at.isoformat(),
+            'updated_at': new_message.updated_at.isoformat()
+        }), 201
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get_or_404(id)
+    
+    if request.method == 'PATCH':
+        data = request.get_json()
+        message.body = data['body']
+        db.session.commit()
+        
+        return jsonify({
+            'id': message.id,
+            'body': message.body,
+            'username': message.username,
+            'created_at': message.created_at.isoformat(),
+            'updated_at': message.updated_at.isoformat()
+        })
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return '', 204
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=4000, debug=True)
